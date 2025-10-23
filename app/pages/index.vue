8c4e3a52dd5cda42e00e4bbf0d8d4cd41cb00318
Edouard@7.fr <edouard@7.fr>
2025-10-23 22:03:39 +0200

    feat: update content structure and add blog collection with initial posts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,8 @@
 </template>
 
 <script setup lang="ts">
-const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
-
+//const { data: home } = await useAsyncData(() => queryCollection('content').path('/index-z').first())
+const { data: home } = await useAsyncData(() => queryCollection('blog').path('/posts/').first())
 useSeoMeta({
     title: home.value?.title,
     description: home.value?.description
